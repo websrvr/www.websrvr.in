0bd4d79109b2020745cfa94dcfd3a87ff38d5cbc
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2014-09-12 01:23:05 +0530

    Added more pages

--- config.rb
+++ config.rb
@@ -54,6 +54,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :build_dir, '/home/minhajuddin/Dropbox/Apps/websrvr/wwww'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
